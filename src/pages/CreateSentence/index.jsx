59cf96c4e21ca0d2ec1519208ef6a106bf45dabf
khushijpatel <kjp002@ucsd.edu>
2023-09-11 23:06:27 -0700

    add homepage and add sentence page

---
+++ src/pages/CreateSentence/index.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import axios from "axios";
+import styles from './style.module.css';
+
+const InputSentenceComponent =  () =>  {
+    const [sentence, setSentence] = useState(""); // make a default empty string state to avoid getting undefined objs 
+
+    const handleInputChange = (event) => {
+        setSentence(event.target.value);
+    } 
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          await axios.post("http://localhost:5000/sentenceBank/sentence", {
+            sentence,
+          });
+          console.log("Sentence successfully submitted!");
+          // Optionally, you can reset the input field after successful submission
+          setSentence("");
+        } catch (error) {
+          console.error("Failed to submit sentence:", error);
+        }
+    };
+
+
+
+
+  return (
+    <form class={styles.create_sentence} onSubmit={handleSubmit}>
+      <div class={styles.sentence_form}>
+          <h1>Create a Sentence</h1>
+          <div class={styles.form_row}>
+              <p><label for="text">Sentence:</label></p>
+              <input
+                type="text"
+                class={styles.sentence_input}
+                value={sentence}
+                onChange={handleInputChange}
+                placeholder="Enter your sentence"
+              />
+          </div>
+          <button id={styles.sentence_submission} type="submit">Submit</button>
+      </div>
+    </form>
+  );
+};
+
+
+export default InputSentenceComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div className={styles.welcome}>
+          <h2>Welcome to HackRacer!!</h2>
+          <p>You can navigate to <i>Add Sentence</i> button at the top to log a new purchase. 
+              Then you can go over to <i>Play Game</i> to view the purchases.
+          </p>
+      </div>
+      <div className={styles.image}><Image src={"/acmlogo.png"} width={384} height={384} alt="acmlogo"/>
+      </div>
     </main>
   )
 }
