249a07997e047f342b28988ba7bf6ec1618d69db
Charvi Shukla <67646308+charvishukla@users.noreply.github.com>
2023-09-16 00:30:27 -0700

    added code for frontend, created readme with info about dependencies

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TypingGameComponent from '../components/TypingGameComponent';
+import InputSentenceComponent from '../components/InputSentenceComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hack Racer</h1>
+      <div  className='input-sentence-component'> 
+        <h2>Create custom sentences! </h2>
+        <InputSentenceComponent/>
+      </div>
+      
+      <div className='game-component'>  
+        <h2>Play!</h2>
+        <TypingGameComponent/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
