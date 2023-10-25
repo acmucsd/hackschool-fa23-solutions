15eb7de38691e7344db24d1b9fb463313bc8ecdc
Angela Hu <angelahu925@gmail.com>
2023-10-24 23:04:06 -0700

    Removed unnecessary comments

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,15 +1,10 @@
 import Image from 'next/image';
-import Link from "next/link";
 import styles from '../styles/Home.module.css';
 import acmLogo from '../assets/acmlogo.png';
-// Navigation in Next.js is slightly different from react. 
-// Look at: https://nextjs.org/docs/app/building-your-application/routing/linking-and-navigating
 
 
 function Home() {
   return (
-    // Next uses Link component for prefetching and client-side navigation
-    // which extends the <a> element
     <div className={styles.main}>
         <div className={styles.welcome}>
           <h2>Welcome to HackRacer!</h2>
--- client/src/pages/sentencePage.jsx
+++ client/src/pages/sentencePage.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import InputSentenceComponent from "../components/input-component/InputSentenceComponent";
 
 
