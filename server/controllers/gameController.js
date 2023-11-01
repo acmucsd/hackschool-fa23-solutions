2bd853236cab38679c68ea87d4ba79476da6c54b
Nikhil Dange <nsdange@ucsd.edu>
2023-11-01 15:56:24 -0700

    updated sol code

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,63 +1,35 @@
-// controllers/gameController.js
-// Note: These API requests dont actually save stuff/fetch from the database. These are stubs
-const GameStats = require("../models/gameStats");
+const getGame = async (req, res) => {
 
-// Controller functions
-// simply get all game stats
-const getGameStats = async (req, res) => {
-  try {
-    // for now, we will be returning an array of game stats objects  
-    const gameStats = [
-      {
-        sentence: "This is sentence 1!",
-        correctcharacters: 10,
-        incorrectcharacters: 2,
-        wpm: 45.23,
-        time: 25,
-      },
-      {
-        sentence: "This is sentence 2!",
-        correctcharacters: 11,
-        incorrectcharacters: 3,
-        wpm: 55.01,
-        time: 25,
-      },
-      {
-        sentence: "This is sentence 3!",
-        correctcharacters: 12,
-        incorrectcharacters: 4,
-        wpm: 22.87,
-        time: 25,
-      },
-      {
-        sentence: "This is sentence 4!",
-        correctcharacters: 13,
-        incorrectcharacters: 5,
-        wpm: 1999.2,
-        time: 25,
-      },
+    const game = [
+        {
+            sentence: 'Hello there!',
+            correctCharacters: 12,
+            incorrectCharacters: 0,
+            wpm: 100,
+            time: 13.33
+        }
     ];
-    
-    // console.log(res.json(gameStats)); // print the array of gamestats 
-    res.status(200).json({ gameStats });
-  } catch (error) {
-    // if there is an error, we print an error message with error code 500
-    res.status(500).json({ error: "Failed to retrieve game stats" });
-  }
-};
+
+    res.status(200).json(game);
+}
+
+const postGame = async (req, res) => {
+
+    const { game } = req.body;
+    const { sentence, correctCharacters, incorrectCharacters, wpm, time } = game;
+
+    if (!sentence || !correctCharacters || !incorrectCharacters || !wpm || !time) {
+        res.status(400).json({ error: 'Invalid request' });
+    }
+    const sentenceLength = sentence.length;
+    if (sentenceLength !== correctCharacters + incorrectCharacters) {
+        res.status(400).json({ error: 'Sentence length does not match' });
+    }
+    else {
+        res.status(200).json({ game })
+    }
+}
 
 
-// Creating/Posting a new gamestats object 
-const createGameStats = async (req, res) => {
 
-  try {
-    const {gamestat} = {}
-    res.json(newGameStats);
-  } catch (error) {
-    // or throw error
-    res.status(500).json({ error: "Failed to create a new gamestat object " });
-  }
-};
-
-
-module.exports = { getGameStats, createGameStats }
+module.exports = { getGame, postGame };
--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,10 +1,11 @@
-// routes/gameRoutes.js
 const express = require('express');
 const router = express.Router();
+
 const gameController = require('../controllers/gameController');
 
-// Define routes
-router.get('/game', gameController.getGameStats);
-router.post('/game', gameController.createGameStats);
+// Add API routes here
+router.get('/game', gameController.getGame);
+router.post('/game', gameController.postGame);
+
 
 module.exports = router;
