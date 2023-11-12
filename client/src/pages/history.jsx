a06c579b812f66231d56d54285397f87986da563
AVERGNET <a.v.vergnet@gmail.com>
2023-11-11 19:49:50 -0800

    Updated Solutions

--- client/src/pages/history.jsx
+++ client/src/pages/history.jsx
@@ -1,34 +1,7 @@
-import React, { useState, useEffect } from "react";
 import CardComponent from "@/components/card-component/CardComponent";
-import styles from '../styles/History.module.css';
-import API from "../api/API";
+import styles from '../styles/History.module.css'
 
 export default function History() {
-  const [gameStats, setGameStats] = useState([]);
-  const [top3GameStats, setTop3GameStats] = useState([]);
-
-  useEffect(() => {
-    // Fetch game stats data from your backend API here
-    // You can use fetch or axios to make the API request to getGameStats and getTop3GameStats endpoints
-    // Once you have the data, set it in the state variables gameStats and top3GameStats
-    // For example:
-    API.getGameStats().then((response) => {
-        setGameStats(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Failed to fetch game stats:", error);
-      });
-
-    API.getTopThreeGameStats().then((response) => {
-        setTop3GameStats(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Failed to fetch top 3 game stats:", error);
-      });
-  }, []); // Make sure to provide the appropriate dependency array
-
   return (
     <div className={styles.container}>
       <h1>Previous Games</h1>
@@ -37,33 +10,25 @@ export default function History() {
       <div className={styles.top_three}>
         <h4 className={styles.header}>Top Three Games</h4>
         <div className={styles.top_three_cards}>
-          {top3GameStats.map((game, index) => (
-            <CardComponent
-              key={index}
-              sentence={game.sentence}
-              correctCharacters={game.correctCharacters}
-              incorrectCharacters={game.incorrectCharacters}
-              wpm={game.wpm}
-              time={game.time}
-            />
-          ))}
+          <CardComponent sentence={"A sentence"}/>
+          <CardComponent sentence={"another sentence"}/>
+          <CardComponent sentence={"another another sentence"}/>
         </div>
       </div>
 
-      {/* Container for Other Games */}
+      {/* Container for All Games */}
       <div className={styles.all_games}>
-        <h4 className={styles.header}>All Games</h4>
+        <h4 className={styles.header}> All Games</h4>
         <div className={styles.all_cards}>
-          {gameStats.map((game, index) => (
-            <CardComponent
-              key={index}
-              sentence={game.sentence}
-              correctCharacters={game.correctCharacters}
-              incorrectCharacters={game.incorrectCharacters}
-              wpm={game.wpm}
-              time={game.time}
-            />
-          ))}
+          <CardComponent
+            sentence={"hahahahhah"}
+            correctCharacters={20}
+            incorrectCharacters={34}
+            wpm={42}
+            time={1}/>
+          <CardComponent sentence={"blablabalbal"}/>
+          <CardComponent sentence={"more sentences"}/>
+          <CardComponent sentence={"sentences more"}/>
         </div>
       </div>
     </div>
